ffdb3765ec59ecac4d392aab2370ef51feb71c5f
henry komatsu <komatsuhenry@gmail.com>
2025-09-17 20:52:57 -0300

    feat: add middleware functions for user && user/admin.

--- src/middlewares/roleMiddleware.js
+++ src/middlewares/roleMiddleware.js
@@ -1,16 +1,32 @@
-const roleMiddleware = (req, res, next) => {
-    const role = req.headers["x-role"]; // pega o papel do usuário no header
-    
-    if (!role) {
-      return res.status(400).json({ error: "Role header missing" });
-    }
-  
-    if (role !== "admin" && role !== "student") {
-      return res.status(403).json({ error: "Invalid role" });
-    }
-  
-    req.role = role; // guarda no request para usar depois
-    next();
-  };
+// Função que retorna um middleware para checar um perfil específico.
+const authAdmin = (requiredRole) => {
+  return (req, res, next) => {
+    const userRole = req.headers['role']; // pega o valor do header
 
-module.exports = { roleMiddleware };
+    if (!userRole) {
+      return res.status(401).json({ error: 'role não encontrada' });
+    }
+
+    if (userRole === requiredRole) {
+      next(); // next() permite e passa para a controller
+    } else {
+      return res.status(403).json({ error: 'Rota exclusiva para administradores.' });
+    }
+  };
+};
+
+const authUserAndAdmin = (req, res, next) => {
+  const userRole = req.headers['role'];
+
+  if (userRole && (userRole === 'admin' || userRole === 'user')) { // se user || admin
+    next(); 
+  } else {
+    return res.status(401).json({ error: 'Rota exclusiva para users || admins' });
+  }
+};
+
+
+module.exports = {
+  authAdmin,
+  authUserAndAdmin
+};
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controller/postController');
+const { authAdmin, authUserAndAdmin } = require('../middlewares/roleMiddleware');
+
 
 // rotas CRUD para admin
-router.post('/', postController.createPost);
-router.put('/:id', postController.updatePost);
-router.delete('/:id', postController.deletePost);
+router.post('/', authAdmin('admin'), postController.createPost);
+router.put('/:id', authAdmin('admin'), postController.updatePost);
+router.delete('/:id', authAdmin('admin'), postController.deletePost);
 
-//ambos
-router.get('/', postController.getAllPosts);
-router.get('/search/:string', postController.getPostByString);
-router.get('/:id', postController.getPostById);
+// rotas para ambos os perfis ('admin' e 'user')
+// 3. Aplicamos o middleware isAuthenticated para garantir que o usuário está "logado"
+router.get('/', authUserAndAdmin, postController.getAllPosts);
+router.get('/search/:string', authUserAndAdmin, postController.getPostByString);
+router.get('/:id', authUserAndAdmin, postController.getPostById);
+
 
 
 module.exports = router;
