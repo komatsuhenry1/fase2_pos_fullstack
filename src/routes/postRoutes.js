1f8f1504fd7d474aa4a83bf0308610d24813f95d
murilojms <murilojms7@gmail.com>
2025-11-24 17:32:24 -0300

    add cors

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,7 +2,13 @@ const express = require('express');
 const router = express.Router();
 const postController = require('../controller/postController');
 const { authAdmin, authUserAndAdmin } = require('../middlewares/roleMiddleware');
-
+const cors = require('cors');
+// colocar cors 
+router.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
 
 // rotas CRUD para admin
 router.post('/', authAdmin('admin'), postController.createPost);
