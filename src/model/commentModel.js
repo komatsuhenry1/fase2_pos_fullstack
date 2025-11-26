162699731d48a45e0c8b019a5eac2e47a745e09f
henry komatsu <komatsuhenry@gmail.com>
2025-11-26 15:31:12 -0300

    refactor: add comment route

--- src/model/commentModel.js
+++ src/model/commentModel.js
@@ -4,34 +4,28 @@ const { v4: uuidv4 } = require('uuid');
 const TABLE = "comments";
 
 const CommentModel = {
-    async createComment(postId, data) {
-        const id = uuidv4();
+    async createComment(post_id, data) {
+        const comment_id = uuidv4();
+
         const now = new Date().toISOString();
 
-        const postData = {
-            id: id,
-            post_id: postId,
+        const commentData = {
+            id: comment_id,
+            post_id: post_id,
             user_id: data.user_id,
             name: data.name,
             comment: data.comment,
             created_at: now
-        };
+        }
 
         const { data: comment, error } = await supabase
             .from(TABLE)
-            .insert([postData])
+            .insert([commentData])
             .select();
-
-        if (error) {
-            throw new Error(`Supabase Error: ${error.message}`);
-        }
-
-        if (!comment || comment.length === 0) {
-            throw new Error('Error creating comment: No data returned');
-        }
-
+        if (error) throw error;
         return comment[0];
     },
-};
+
+}
 
 module.exports = { CommentModel };
